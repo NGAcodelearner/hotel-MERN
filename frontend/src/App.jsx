9dda2f7d5975e62f1c20a7bcba2afbdb51c5ed7d
NGA <r0917268@student.thomasmore.be>
2024-05-14 12:38:49 +0700

    set up routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Hotel from "./pages/Hotel";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UpdateHotel from "./pages/UpdateHotel";
+
 const App = () => {
-  return <div className="bg-red-400">App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/:id/update" element={<UpdateHotel />} />
+        <Route path="/:id" element={<Hotel />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
